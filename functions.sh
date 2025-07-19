b5952f7063f37aaa291af626fb85c75b9961a369
Daniel Kaiser <dankaiser1808@gmail.com>
2025-07-19 14:40:04 -0500

    add helper functions

--- functions.sh
+++ functions.sh
@@ -1,10 +1,13 @@
 #!/bin/bash
 
-BACKEND_BUCKET_NAME="dk-arch-backup-tf-state"
+BACKEND_BUCKET_NAME="dk-arch-backup-tf-state-test"
+REGION="us-east-1"
+BUCKET_VERSIONING="Enabled"
 
 create-backend-bucket() {
     if command -v aws >/dev/null 2>&1; then
-        aws s3api create-bucket --bucket "$BACKEND_BUCKET_NAME"
+        aws s3api create-bucket --bucket "$BACKEND_BUCKET_NAME" --region "$REGION"
+        aws s3api put-bucket-versioning --bucket "$BACKEND_BUCKET_NAME" --versioning-configuration Status="$BUCKET_VERSIONING"
     else
         echo "AWS CLI not installed."
     fi
